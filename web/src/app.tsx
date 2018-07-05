bcf2e0f2d89b1cc2c8a77af335bee51979aa222e
Gabriel Wu <qqbbnease1004@126.com>
2018-07-05 20:43:55 +0800

    glide init

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,9 +1,13 @@
 import * as Rx from 'rxjs'
-import {Sources, Sinks} from './interfaces'
+import { Sources, Sinks } from './interfaces'
+import { pluck } from 'rxjs/operator/pluck';
 
-export function App(sources : Sources) : Sinks {
+export function App(sources: Sources): Sinks {
   const vtree$ = Rx.Observable.of(
-    <div>My Awesome Cycle.js app</div>
+    <div>
+      <h1>My Awesome Cycle.js app</h1>
+      <h2>Created by Gabriel Wu</h2>
+    </div>
   )
 
   return {
